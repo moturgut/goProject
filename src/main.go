9438bbca301ea67ff86aafb080112bffa917a90b
moturgut <mehmetokanturgut@gmail.com>
2020-07-20 16:40:47 +0300

    test

--- src/main.go
+++ src/main.go
@@ -3,13 +3,12 @@ package main
 import (
 	"goproject/app"
 	"goproject/config"
-	"os"
 )
 
 func main() {
 	config := config.GetConfig()
-	port := os.Getenv("PORT")
+
 	app := &app.App{}
 	app.Initialize(config)
-	app.Run(":" + port)
+	app.Run(":8116")
 }
